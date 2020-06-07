39661c04e68adcd8d9af1085985298e4b61f229c
Maria <maria@2-faces.ru>
2020-06-07 21:01:35 +0300

    Add logger

--- src/main/java/org/s2f/mb/service/db/DatabaseHandler.java
+++ src/main/java/org/s2f/mb/service/db/DatabaseHandler.java
@@ -1,16 +1,22 @@
 package org.s2f.mb.service.db;
 
 import org.s2f.mb.model.dto.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 
 public class DatabaseHandler {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseHandler.class);
 
     public void addProduct(Product p) {
         String insert = "INSERT INTO food (name, weight, price, priceByOneGramm, kcal, isCooked) VALUES (?,?,?,?,?,?)";
 
         try {
             PreparedStatement prSt = DBConnection.getInstance().prepareStatement(insert);
+
+            log.info("Connected to DB.");
+
             System.out.println("Connection!!!");
             prSt.setString(1, p.getName());
             prSt.setInt(2, p.getWeight());
@@ -23,6 +29,7 @@ public class DatabaseHandler {
 
             prSt.close();
             System.out.println("Connection is closed.");
+            log.info("Connection is closed.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
