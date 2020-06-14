0c59245dca0168bacb316f1ae5df81e19084017e
Maria <maria@2-faces.ru>
2020-06-14 15:26:16 +0300

    Add fridge_counter_db.txt

--- src/main/java/org/s2f/mb/service/db/DBConnection.java
+++ src/main/java/org/s2f/mb/service/db/DBConnection.java
@@ -16,7 +16,7 @@ public class DBConnection {
 
     public static synchronized Connection getInstance() {
         if (dbc == null) {
-            String connectionString = "jdbc:mysql://localhost:3306/fridge_counter_db?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
+            String connectionString = "jdbc:mysql://localhost:3306/fridge_counter_db.txt?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 dbc = DriverManager.getConnection(connectionString, "root", "root");
