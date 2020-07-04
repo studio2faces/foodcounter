f2b41029bfdc3da32f0aaff0da3f47f625cf4a5e
Maria <maria@2-faces.ru>
2020-07-04 16:24:54 +0300

    Some changes

--- src/main/java/org/s2f/mb/servlets/AddAndShowServlet.java
+++ src/main/java/org/s2f/mb/servlets/AddAndShowServlet.java
@@ -10,6 +10,7 @@ import org.s2f.mb.service.mappers.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
