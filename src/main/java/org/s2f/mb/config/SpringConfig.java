f24f83d583d9605f91e02cf1aace0d17c881fa59
Maria <maria@2-faces.ru>
2020-08-08 18:09:44 +0300

    Modify config

--- src/main/java/org/s2f/mb/config/SpringConfig.java
+++ src/main/java/org/s2f/mb/config/SpringConfig.java
@@ -5,7 +5,6 @@ import org.s2f.mb.service.interceptors.AuthorizationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
